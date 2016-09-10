fb9683f202566e0dba23cf28ff013c10a7ce1516
trujunzhang <trujunzhang@gmail.com>
2016-09-10 15:59:43 +0800

    add nova-tags package.

--- packages/nova-base-components/lib/common/Header.jsx
+++ packages/nova-base-components/lib/common/Header.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import {ListContainer, DocumentContainer} from "meteor/utilities:react-list-container";
 //import { Messages } from "meteor/nova:core";
 
 const Header = ({currentUser}) => {
 
-  //const logoUrl = Telescope.settings.get("logoUrl");
-  //const siteTitle = Telescope.settings.get("title", "Nova");
-  //const tagline = Telescope.settings.get("tagline");
+    //const logoUrl = Telescope.settings.get("logoUrl");
+    //const siteTitle = Telescope.settings.get("title", "Nova");
+    //const tagline = Telescope.settings.get("tagline");
 
-  return (
-    <div className="header_2k8Jf">
-      <div className="constraintWidth_ZyYbM">
-        <Telescope.components.HeaderContent currentUser={currentUser}/>
-        <Telescope.components.HeaderNavigation/>
+    return (
+      <div className="header_2k8Jf">
+          <div className="constraintWidth_ZyYbM">
+
+              <Telescope.components.HeaderContent currentUser={currentUser}/>
+
+              <ListContainer
+                collection={Categories}
+                resultsPropName="categories"
+                limit={0}
+                component={Telescope.components.HeaderNavigation}
+                loading={<div ></div>}
+              />
+
+          </div>
       </div>
-    </div>
-  )
+    )
 };
 
 Header.displayName = "Header";
--- packages/nova-base-components/lib/common/HeaderNavigation.jsx
+++ packages/nova-base-components/lib/common/HeaderNavigation.jsx
@@ -6,26 +6,23 @@ import {Modal, Dropdown, MenuItem} from 'react-bootstrap';
 import {ContextPasser} from "meteor/nova:core";
 import {LinkContainer} from 'react-router-bootstrap';
 import Users from 'meteor/nova:users';
+import {withRouter} from 'react-router'
 
 class HeaderNavigation extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-
-            subNavigationItems: [
-                {"href": "/tech", "title": "Politics"},
-                {"href": "/games", "title": "Economy"},
-                {"href": "/podcasts", "title": "Foreign Affairs"},
-                {"href": "/books", "title": "Defence"},
-                {"href": "/topics/developer-tools", "title": "Education"},
-                {"href": "/topics/photography-tools", "title": "Healthcare"},
-                {"href": "/topics/wearables", "title": "Sustainability"},
-            ]
-        }
     }
 
     render() {
+        const categories = this.props.categories;
+
+        //const {category, index, router} = this.props;
+        //
+        const currentQuery = router.location.query;
+        const currentCategorySlug = router.location.query.cat;
+        const newQuery = _.clone(router.location.query);
+
         return (
           <nav className="navigation_1H-Yv text_3Wjo0 subtle_1BWOT base_3CbW2">
               <a className="item_1k3Lx itemActive_3HLKr item_1k3Lx" href="/">Home</a>
@@ -33,11 +30,9 @@ class HeaderNavigation extends Component {
                   <div className="gradientLeft_33bxf gradient_fDMJD"></div>
                   <div className="gradientRight_Rp6ob gradient_fDMJD"></div>
                   <ol>
-                      {this.state.subNavigationItems.map((menu, key) => {
-                          return (
-                            <li key={key}><a className="item_1k3Lx" href="{menu.href}">{menu.title}</a></li>
-                          )
-                      })}
+                      {categories && categories.length > 0 ? categories.map((category, index) =>
+                        <li key={index}><a className="item_1k3Lx" href="/?cat={category.slug}">{category.name}</a></li>
+                      ) : null}
                       <li>
                           <a className="all_P8Pm- item_1k3Lx secondaryText_PM80d default_tBeAo base_3CbW2"
                              href="/topics">See all</a>
@@ -54,7 +49,7 @@ class HeaderNavigation extends Component {
                               <path
                                 d="M1.05833636,0 L0,1.05654696 L6.95502928,8 L6.64746554e-16,14.943453 L1.05833636,16 L8.54271718,8.52827348 C8.83496729,8.23651377 8.83496729,7.7634825 8.54271718,7.47172652 L1.05833636,0 L1.05833636,0 Z"
                                 fill="#999" transform="matrix(-1 0 0 1 8.762 0)"
-                                ></path>
+                              ></path>
                           </svg>
                       </span>
                   </a>
@@ -64,7 +59,7 @@ class HeaderNavigation extends Component {
                                xmlns="http://www.w3.org/2000/svg">
                               <path
                                 d="M1.05833636,0 L0,1.05654696 L6.95502928,8 L6.64746554e-16,14.943453 L1.05833636,16 L8.54271718,8.52827348 C8.83496729,8.23651377 8.83496729,7.7634825 8.54271718,7.47172652 L1.05833636,0 L1.05833636,0 Z"
-                                fill="#999" ></path>
+                                fill="#999"></path>
                           </svg>
                       </span>
                   </a>
@@ -75,5 +70,5 @@ class HeaderNavigation extends Component {
 
 }
 
-module.exports = HeaderNavigation;
-export default HeaderNavigation;
+module.exports = withRouter(HeaderNavigation);
+export default withRouter(HeaderNavigation);
