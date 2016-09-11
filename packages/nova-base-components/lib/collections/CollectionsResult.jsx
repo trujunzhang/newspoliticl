a61aa4bfc6896aaff2f5646742aadf0585d0193a
trujunzhang <trujunzhang@gmail.com>
2016-09-11 13:31:53 +0800

    add nova-tags copy from nova-categories.

---
+++ packages/nova-base-components/lib/collections/CollectionsResult.jsx
@@ -0,0 +1,21 @@
+import Telescope from 'meteor/nova:lib';
+import React, {PropTypes, Component} from 'react';
+import {Button} from 'react-bootstrap';
+import moment from 'moment';
+
+class CollectionsResult extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+          <div className="posts-daily">
+          </div>
+        )
+    }
+}
+
+module.exports = CollectionsResult;
+export default CollectionsResult;
