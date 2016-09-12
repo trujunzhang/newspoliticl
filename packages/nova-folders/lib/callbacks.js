7bad061d1a97d834d866324be38cb40f2d9274d7
trujunzhang <trujunzhang@gmail.com>
2016-09-12 10:18:41 +0800

    add nova-tags copy from nova-categories.

--- packages/nova-folders/lib/callbacks.js
+++ packages/nova-folders/lib/callbacks.js
@@ -16,7 +16,70 @@ Folders.before.update(function (userId, doc, fieldNames, modifier) {
     }
 });
 
+// ------------------------------------- folders.new.method -------------------------------- //
+
+function FoldersNewUserCheck (folder, user) {
+    // check that user can post
+    if (!user || !Users.canDo(user, "folders.new"))
+        throw new Meteor.Error(601, 'you_need_to_login_or_be_invited_to_post_new_folders');
+    return folder;
+}
+Telescope.callbacks.add("folders.new.method", FoldersNewUserCheck);
+
+
+function FoldersNewSubmittedPropertiesCheck (folder, user) {
+    // admin-only properties
+    // userId
+    const schema = Folders.simpleSchema()._schema;
+
+    // clear restricted properties
+    //_.keys(folder).forEach(function (fieldName) {
+    //
+    //    // make an exception for postId, which should be setable but not modifiable
+    //    if (fieldName === "postId") {
+    //        // ok
+    //    } else {
+    //        var field = schema[fieldName];
+    //        if (!Users.canSubmitField (user, field)) {
+    //            throw new Meteor.Error("disallowed_property", 'disallowed_property_detected' + ": " + fieldName);
+    //        }
+    //    }
+    //
+    //});
+
+    // if no userId has been set, default to current user id
+    if (!folder.userId) {
+        folder.userId = user._id;
+    }
+    return folder;
+}
+Telescope.callbacks.add("folders.new.method", FoldersNewSubmittedPropertiesCheck);
+
+
 // ------------------------------------- folders.new.async -------------------------------- //
+
+/**
+ * @summary Check for required properties
+ */
+function FoldersNewRequiredPropertiesCheck (folder, user) {
+
+    var userId = folder.userId; // at this stage, a userId is expected
+
+    // Don't allow empty comments
+    if (!folder.name)
+        throw new Meteor.Error(704, 'your_name_is_empty');
+
+    var defaultProperties = {
+        createdAt: new Date(),
+        postedAt: new Date(),
+    };
+
+    folder = _.extend(defaultProperties, folder);
+
+    return folder;
+}
+Telescope.callbacks.add("comments.new.sync", FoldersNewRequiredPropertiesCheck);
+
 
 function FoldersNewOperations(folder) {
 
