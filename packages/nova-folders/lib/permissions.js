b8eea8e51957836031996ea3fb8ca2090f4b5e67
trujunzhang <trujunzhang@gmail.com>
2016-09-12 14:13:46 +0800

    add nova-tags copy from nova-categories.

--- packages/nova-folders/lib/permissions.js
+++ packages/nova-folders/lib/permissions.js
@@ -1,19 +1,17 @@
 import Users from 'meteor/nova:users';
 
-const anonymousActions = [
-  "folders.view.all"
-];
+const anonymousActions = [];
 Users.groups.anonymous.can(anonymousActions);
 
 const defaultActions = [
-  "folders.view.all"
+    "folders.view.all",
+    "folders.view.approved.all",
+    "folders.view.all",
+    "folders.new",
+    "folders.edit.all",
+    "folders.remove.all"
 ];
 Users.groups.default.can(defaultActions);
 
-const adminActions = [
-  "folders.view.all",
-  "folders.new",
-  "folders.edit.all",
-  "folders.remove.all"
-];
+const adminActions = [];
 Users.groups.admins.can(adminActions);
