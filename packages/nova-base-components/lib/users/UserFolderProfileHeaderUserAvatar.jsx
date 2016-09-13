1c65f06051ed1754019d245c50cfaf4c40fcd8e4
trujunzhang <trujunzhang@gmail.com>
2016-09-13 21:16:38 +0800

    add "folders.list".

--- packages/nova-base-components/lib/users/UserFolderProfileHeaderUserAvatar.jsx
+++ packages/nova-base-components/lib/users/UserFolderProfileHeaderUserAvatar.jsx
@@ -7,20 +7,17 @@ class UserFolderProfileHeaderUserAvatar extends Component {
 
     render() {
         const user = this.props.user;
-        //const avatarUrl = Users.avatar.getUrl(user);
-        //const avatar = avatarUrl
-        //    ? <img height="140" width="140" src={avatarUrl}/>
-        //    : "";
-        //
-        //const upvotedPosts = user.telescope.upvotedPosts;
-        //const collections = 0;
+        const avatarUrl = Users.avatar.getUrl(user);
+        const avatar = avatarUrl
+          ? <img className="placeholder_E_0qw placeholderHidden_pb7Bz" height="30" src={avatarUrl} width="30"/>
+          : "";
 
         return (
-            <span className="user-image">
+          <span className="user-image">
                 <div className="container_22rD3 user-image--image">
-                    <div className="container__Ql6q lazyLoadContainer_3KgZD"><img height="30" src="./Product Hunt_files/original(2)" srcset="https://ph-avatars.imgix.net/641022/original?auto=format&amp;codec=mozjpeg&amp;cs=strip&amp;w=30&amp;h=30&amp;fit=crop&amp;dpr=2 2x, https://ph-avatars.imgix.net/641022/original?auto=format&amp;codec=mozjpeg&amp;cs=strip&amp;w=30&amp;h=30&amp;fit=crop&amp;dpr=3 3x" width="30"/>
+                    <div className="container__Ql6q lazyLoadContainer_3KgZD">
+                        {avatar}
                     </div>
-                    <img className="placeholder_E_0qw placeholderHidden_pb7Bz" height="30" src="./Product Hunt_files/original(1)" width="30"/>
                 </div>
             </span>
         )
