3d86268b77fb38a40ebd85a0910daadef928c77a
trujunzhang <trujunzhang@gmail.com>
2016-09-13 14:26:48 +0800

    add "folders.list".

--- packages/nova-base-components/lib/collections/FoldersItem.jsx
+++ packages/nova-base-components/lib/collections/FoldersItem.jsx
@@ -12,22 +12,25 @@ class FoldersItem extends Component {
 
     onFolderItemClick() {
         const folder = this.props.folder;
+        const currentPathName = this.props.router.location.pathname;
+        const path = currentPathName + "/" + folder.slug;
 
+        this.props.router.push({pathname: path});
     }
 
     render() {
         const folder = this.props.folder;
 
         return (
-            <div className="backgroundImage_1hK9M card_2nuIG card_3kZOV">
-                <a className="link_1QbEt" onClick={this.onFolderItemClick.bind(this)}>
-                    <span className="name_3GvIR featured_2W7jd inverse_1CN6F base_3CbW2">{folder.name}</span>
-                    <span className="title__hjS3 text_3Wjo0 inverse_1CN6F base_3CbW2"></span>
-                </a>
-                <button className="button_2I1re smallSize_1da-r secondaryText_PM80d simpleVariant_1Nl54 follow_3OEqn">
-                    <div className="buttonContainer_wTYxi">{folder.posts.length + " posts"}</div>
-                </button>
-            </div>
+          <div className="backgroundImage_1hK9M card_2nuIG card_3kZOV">
+              <a className="link_1QbEt" onClick={this.onFolderItemClick.bind(this)}>
+                  <span className="name_3GvIR featured_2W7jd inverse_1CN6F base_3CbW2">{folder.name}</span>
+                  <span className="title__hjS3 text_3Wjo0 inverse_1CN6F base_3CbW2"></span>
+              </a>
+              <button className="button_2I1re smallSize_1da-r secondaryText_PM80d simpleVariant_1Nl54 follow_3OEqn">
+                  <div className="buttonContainer_wTYxi">{folder.posts.length + " posts"}</div>
+              </button>
+          </div>
         )
     }
 }
