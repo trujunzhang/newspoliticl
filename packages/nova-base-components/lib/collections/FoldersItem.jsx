b0d53e4a7d2b9023e1dd27b3b6c1f722998f13fb
trujunzhang <trujunzhang@gmail.com>
2016-09-13 13:55:32 +0800

    add "folders.list".

--- packages/nova-base-components/lib/collections/FoldersItem.jsx
+++ packages/nova-base-components/lib/collections/FoldersItem.jsx
@@ -14,30 +14,14 @@ class FoldersItem extends Component {
         const folder = this.props.folder;
 
         return (
-          <div className="postItem_2pV9v" rel="post-item-#74101">
-
-              <a className="link_3fUGJ">
-                  <div className="post-thumbnail thumbnail_JX64A thumbnail post-left-thumbnail">
-                      <div className="container_22rD3 thumbnail">
-                          {folder.thumbnailUrl ? <Telescope.components.PostsThumbnail post={folder}/> : null}
-                      </div>
-                  </div>
-
-                  <div className="content_3oLx4">
-                      <span onClick={this.popupDetail.bind(this)}
-                            className="title_2p9fd featured_2W7jd default_tBeAo base_3CbW2 post-title">{folder.title}</span>
-                      {this.renderAdminActions()}
-                      <p style={{"margin-bottom": 10}} className="post_description"
-                         onClick={this.popupDetail.bind(this)}>
-                          {((mytextvar).length > maxlimit)
-                            ? (((mytextvar).substring(0, maxlimit - 3)) + '...')
-                            : mytextvar}</p>
-                      {this.renderTags()}
-                  </div>
-
-              </a>
-
-              {this.renderActionButtons(folder)}
+          <div className="backgroundImage_1hK9M card_2nuIG card_3kZOV">
+              <a className="link_1QbEt"
+                 href="https://www.producthunt.com/@trujunzhang/collections/imessage-articles-collections-folder-djzhang-wanghao-trujunzhang-ios"><span
+                className="name_3GvIR featured_2W7jd inverse_1CN6F base_3CbW2">imessage articles collections folder djzhang wanghao trujunzhang ios</span><span
+                className="title__hjS3 text_3Wjo0 inverse_1CN6F base_3CbW2"></span></a>
+              <button className="button_2I1re smallSize_1da-r secondaryText_PM80d simpleVariant_1Nl54 follow_3OEqn">
+                  <div className="buttonContainer_wTYxi">Follow</div>
+              </button>
           </div>
         )
     }
--- packages/nova-base-components/lib/collections/FoldersList.jsx
+++ packages/nova-base-components/lib/collections/FoldersList.jsx
@@ -2,7 +2,16 @@ import Telescope from 'meteor/nova:lib';
 import React from 'react';
 const moment = require('moment');
 
-const FoldersList = ({results, currentUser, hasMore, ready, count, totalCount, loadMore, showHeader = true}) => {
+const FoldersList = ({
+  results,
+  currentUser,
+  hasMore,
+  ready,
+  count,
+  totalCount,
+  loadMore,
+  showHeader = true
+}) => {
 
     // console.log(results);
     // console.log(ready);
@@ -11,19 +20,31 @@ const FoldersList = ({results, currentUser, hasMore, ready, count, totalCount, l
     // console.log(count);
 
     return (
-      <ul className="collections-popover--collections popover--scrollable-list">
-          {results.map((folder, index) => {
-                return (
-                  <li>
-                      <a className="collections-popover--collection popover--scrollable-list--element"
-                         href="https://www.producthunt.com/#">{folder.name}
-                          <span className="collections-popover--collection--icon v-collect"></span>
-                      </a>
-                  </li>
-                )
-            }
-          )}
-      </ul>
+
+      <main className="content_36o4C">
+          <div>
+              <div className="paddedBox_2UY-S box_c4OJj">
+                  <div className="header_3GFef hideOnSmallScreen_1VjPA">
+                        <span >
+                            <span
+                              className="title_38djq featured_2W7jd default_tBeAo base_3CbW2">{results.length + " Collections"}</span>
+                        </span>
+                  </div>
+                  <div className="content_DcBqe">
+                      <div className="grid_hjrL6">
+
+                          {results.map((folder, index) => {
+                              return (
+                                <Telescope.components.FoldersItem folder={folder}/>
+                              )
+                          })}
+
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </main>
+
     )
 
 };
