f6dc113891f9d8787498077d711d9188406d7eb7
trujunzhang <trujunzhang@gmail.com>
2016-09-16 22:30:20 +0800

    add "folders.list".

--- packages/nova-base-components/lib/common/Layout.jsx
+++ packages/nova-base-components/lib/common/Layout.jsx
@@ -79,9 +79,7 @@ class Layout extends Component {
             terms={params}
             joins={Posts.getJoins()}
             component={Telescope.components.PostsPage}
-            loading={<div className="placeholder_1WOC3">
-                <div className="loader_54XfI animationRotate loader_OEQVm"></div>
-            </div>}
+            loading={<div/>}
           />
         )
     }
