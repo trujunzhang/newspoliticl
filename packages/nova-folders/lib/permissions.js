d9840e19b477618e75962838cf8f7133a4d65567
trujunzhang <trujunzhang@gmail.com>
2016-09-17 19:47:48 +0800

    add "folders.list".

--- packages/nova-folders/lib/permissions.js
+++ packages/nova-folders/lib/permissions.js
@@ -1,21 +1,17 @@
 import Users from 'meteor/nova:users';
 
-const anonymousActions = [
-    "folders.view.all"
-];
+const anonymousActions = [];
 Users.groups.anonymous.can(anonymousActions);
 
 const defaultActions = [
     "folders.view.approved.all",
-    "folders.view.all",
-    "folders.insertPost"
-];
-Users.groups.default.can(defaultActions);
-
-const adminActions = [
+    "folders.insertPost",
     "folders.view.all",
     "folders.new",
     "folders.edit.all",
     "folders.remove.all"
 ];
+Users.groups.default.can(defaultActions);
+
+const adminActions = [];
 Users.groups.admins.can(adminActions);
