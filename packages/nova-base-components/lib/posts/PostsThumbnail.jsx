dc909fe9b0d90387fcc114d911a43976971dce26
trujunzhang <trujunzhang@gmail.com>
2016-09-17 12:42:21 +0800

    add "folders.list".

--- packages/nova-base-components/lib/posts/PostsThumbnail.jsx
+++ packages/nova-base-components/lib/posts/PostsThumbnail.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
 import Posts from "meteor/nova:posts";
-import LazyLoad from 'react-lazy-load';
 
 const PostsThumbnail = ({post}) => {
 
     return (
 
       <div className="container__Ql6q lazyLoadContainer_3KgZD">
-          <LazyLoad >
               <img
                 width="100" height="100"
                 src={Posts.getThumbnailUrl(post)}
                 className="attachment-thumbnail size-thumbnail wp-post-image"
                 alt={post.title}
                 title={post.title}/>
-          </LazyLoad>
       </div>
     )
 };
